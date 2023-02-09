98d3e8cd53fdac651cf16c217477c6f182c09b00
josefli <70848593+josefli@users.noreply.github.com>
2023-02-09 17:37:36 +0100

    Current project
    Project with login + navigation to sign up & applications w/ authorization

---
+++ recruitment/src/main/java/se/kth/iv1201/recruitment/user/UserService.java
@@ -0,0 +1,21 @@
+package se.kth.iv1201.recruitment.user;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserService implements UserDetailsService {
+
+    private final static String USER_NOT_FOUND_MSG = "user with username %s not found";
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username).orElseThrow(() ->
+                new UsernameNotFoundException(String.format(USER_NOT_FOUND_MSG, username)));
+    }
+}
