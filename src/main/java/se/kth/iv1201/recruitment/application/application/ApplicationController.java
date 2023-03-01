8b697e49af65760fc2c2705c07aa0089cfd91d73
josefli <70848593+josefli@users.noreply.github.com>
2023-03-01 16:20:59 +0100

    Add files via upload
    Added application classes

---
+++ src/main/java/se/kth/iv1201/recruitment/application/application/ApplicationController.java
@@ -0,0 +1,40 @@
+package se.kth.iv1201.recruitment.application;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+/**
+ * Handles HTTP requests for applications.
+ */
+@Controller
+public class ApplicationController {
+
+    @Autowired
+    private ApplicationRepository applicationRepo;
+
+    /**
+     * No page is specified, show home page.
+     *
+     * @return A response that shows index page url.
+     */
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    /**
+     * A get request to view applications page. This page can only be accessed by recruiters.
+     *
+     * @return The applications' web page url.
+     */
+    @GetMapping("/applications")
+    public String listAllApplications(Model model) {
+        model.addAttribute("applications", applicationRepo.findAll());
+        System.out.println(applicationRepo.findAll());
+        return "applications";
+    }
+
+}
