ddcd4add2648cf3bbe3cc474dae280fbc38feb4e
Josefin Lindström <josefli@kth.se>
2023-03-01 16:46:32 +0100

    Added javadoc comments

--- src/main/java/se/kth/iv1201/recruitment/application/ApplicationRepository.java
+++ src/main/java/se/kth/iv1201/recruitment/application/ApplicationRepository.java
@@ -3,13 +3,19 @@ package se.kth.iv1201.recruitment.application;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 
+/**
+ * Contains all database access regarding applications.
+ */
 @Repository
 public interface ApplicationRepository extends JpaRepository<Application, Long>{
 
+	/**
+	 * Returns a list of type Applications from database.
+	 *
+	 * @return all applications in database
+	 */
 	List<Application> findAll();
 
 }
