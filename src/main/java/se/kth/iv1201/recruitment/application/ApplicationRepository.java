b345457310926d6b55e51cd92c7ce9f43629dcb6
originalpau <pauhua@kth.se>
2023-03-10 11:38:40 +0100

    Update ApplicationRepository.java for transaction

--- src/main/java/se/kth/iv1201/recruitment/application/ApplicationRepository.java
+++ src/main/java/se/kth/iv1201/recruitment/application/ApplicationRepository.java
@@ -3,12 +3,19 @@ package se.kth.iv1201.recruitment.application;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
 /**
  * Contains all database access regarding applications.
+ *
+ * A transaction must already exist when the methods in this class is called.
+ * If a transaction does not already exist, a runtime exception will be thrown.
  */
 @Repository
+@Transactional(propagation = Propagation.MANDATORY)
 public interface ApplicationRepository extends JpaRepository<Application, Long>{
 
 	/**
